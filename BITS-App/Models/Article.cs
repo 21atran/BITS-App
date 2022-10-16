adb2b1480121c7aa1b0e79d37de32c67df42057b
MandarinPine18 <59212466+MandarinPine18@users.noreply.github.com>
2022-10-15 22:59:10 -0500

    Added partial manual deserialization of Post jsons

--- BITS-App/Models/Article.cs
+++ BITS-App/Models/Article.cs
@@ -1,3 +1,4 @@
+﻿using Newtonsoft.Json;
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,21 +8,40 @@ using WordPressPCL;
 
 namespace BITS_App.Models
 {
+    internal class Post {
+        public int id { get; set; }
+        public DateTime date { get; set; }
+        public DateTimeOffset date_gmt { get; set; }
+        public Dictionary<string, string> title { get; set; }
+        public CustomFields custom_fields { get; set; }
+    }
+
+    internal class CustomFields {
+        public string[] writer { get; set; }
+    }
+
+
     internal class Article
     {
         WordPressClient client;
         private int id;
         WordPressPCL.Models.Post post;
+        string raw;
+        Post postJson;
 
         public Article(int id)
         {
             client = new WordPressClient("https://gwhsnews.org/wp-json/");
             this.id = id;
 
+            raw = "{}";
+            client.HttpResponsePreProcessing = (response) => raw = response;
+
             var task = client.Posts.GetByIDAsync(id);
             task.Wait();
             post = task.Result;
 
+            postJson = JsonConvert.DeserializeObject<Post>(raw);
         }
 
         public string Title => post.Title.Rendered;
