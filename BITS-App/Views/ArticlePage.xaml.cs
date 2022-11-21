d63867609aec736d9ca566b5dd322319dd067134
Galaxy <grkalinich@gmail.com>
2022-11-20 19:19:04 -0600

    Showed Image, Text, and Image Caption

--- BITS-App/Views/ArticlePage.xaml.cs
+++ BITS-App/Views/ArticlePage.xaml.cs
@@ -1,5 +1,4 @@
 using HtmlAgilityPack;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace BITS_App.Views;
 
@@ -14,16 +13,52 @@ public partial class ArticlePage : ContentPage
         var html = ((Models.Article)BindingContext).Content;
         var htmlDoc = new HtmlDocument();
         htmlDoc.LoadHtml(html);
-        var parNodes = htmlDoc.DocumentNode.SelectNodes("/p");
-        foreach (var node in parNodes)
+        var parNodes = htmlDoc.DocumentNode.SelectNodes("/");
+        foreach (var node in parNodes.Nodes())
         {
-            Label label = new Label()
+            StackLayout chlidLayout = new StackLayout();
+            if (node.Name=="p")
             {
-                Text = node.InnerText,
-                FontSize = 14
-                // can format here
-            };
-            mainText.Children.Add(label);
+                Label label = new Label()
+                {
+                    Text = node.InnerText,
+                    FontSize = 14
+                    // can format here
+                };
+                chlidLayout.Add(label);
+                mainText.Children.Add(chlidLayout);
+            }
+            else if (node.Name=="#text")
+            {
+                Label label = new Label()
+                {
+                    Text = "",
+                    FontSize = 7
+                    // can format here
+                };
+                chlidLayout.Add(label);
+                mainText.Children.Add(chlidLayout);
+            }
+            else if (node.Name=="figure")
+            {
+                Image img = new Image()
+                {
+                    Source = node.SelectSingleNode("//img").Attributes["src"].Value.ToString()
+                    // can format here
+                };
+                Label label = new Label()
+                {
+                    Padding = 10,
+                    Text = node.SelectSingleNode("//figcaption").InnerText,
+                    FontSize = 10.5,
+                    TextColor = Color.FromArgb("#000000"),
+                    BackgroundColor = Color.FromArgb("#FFFFFF")
+                    // can format here
+                };
+                chlidLayout.Add(img);
+                chlidLayout.Add(label);
+                mainText.Children.Add(chlidLayout);
+            }
         }
     }
 
