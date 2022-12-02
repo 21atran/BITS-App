18bb0767b6c7ad6599bd5ac7a52e8332b76556e8
Galaxy <grkalinich@gmail.com>
2022-12-02 15:17:05 -0600

    Made loadable content of an article into an async function.

--- BITS-App/Views/ArticlePage.xaml.cs
+++ BITS-App/Views/ArticlePage.xaml.cs
@@ -5,19 +5,32 @@ namespace BITS_App.Views;
 public partial class ArticlePage : ContentPage
 {
     public ArticlePage()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
 
         BindingContext = new Models.Article(7767);
 
-        var html = ((Models.Article)BindingContext).Content;
-        var htmlDoc = new HtmlDocument();
+        Dispatcher.Dispatch(async () => await LoadContentAsync());
+    }
+
+    /// <summary>
+    /// Loads content to XAML from HTML Content binding of <see cref="Models.Article">Models.Article</see>
+    /// </summary>
+    public async Task LoadContentAsync()
+    {
+        // gets html in string format
+        string html = ((Models.Article)BindingContext).Content;
+
+        // uses agility pack and creates doc with string
+        HtmlDocument htmlDoc = new HtmlDocument();
         htmlDoc.LoadHtml(html);
-        var parNodes = htmlDoc.DocumentNode.SelectNodes("/");
-        foreach (var node in parNodes.Nodes())
+
+        // gets the nodes
+        HtmlNodeCollection parNodes = htmlDoc.DocumentNode.SelectNodes("/");
+        foreach (HtmlNode node in parNodes.Nodes())
         {
-            StackLayout chlidLayout = new StackLayout();
-            if (node.Name=="p")
+            // if node is named p then it is a paragraph
+            if (node.Name == "p")
             {
                 Label label = new Label()
                 {
@@ -25,22 +38,24 @@ public partial class ArticlePage : ContentPage
                     FontSize = 14
                     // can format here
                 };
-                chlidLayout.Add(label);
-                mainText.Children.Add(chlidLayout);
+                // adds the paragraph to document
+                contentStackLayout.Add(label);
             }
-            else if (node.Name=="#text")
+            else if (node.Name == "#text")
             {
+                // if node is named #text then that is a blank line (a enter)
                 Label label = new Label()
                 {
                     Text = "",
                     FontSize = 7
                     // can format here
                 };
-                chlidLayout.Add(label);
-                mainText.Children.Add(chlidLayout);
+                contentStackLayout.Add(label);
             }
-            else if (node.Name=="figure")
+            else if (node.Name == "figure")
             {
+                // this gets the image details of both the image and the caption
+                StackLayout chlidLayout = new StackLayout();
                 Image img = new Image()
                 {
                     Source = node.SelectSingleNode("//img").Attributes["src"].Value.ToString()
@@ -51,13 +66,13 @@ public partial class ArticlePage : ContentPage
                     Padding = 10,
                     Text = node.SelectSingleNode("//figcaption").InnerText,
                     FontSize = 10.5,
-                    TextColor = Color.FromArgb("#000000"),
-                    BackgroundColor = Color.FromArgb("#FFFFFF")
+                    BackgroundColor = Colors.Transparent
                     // can format here
                 };
                 chlidLayout.Add(img);
                 chlidLayout.Add(label);
-                mainText.Children.Add(chlidLayout);
+                // adds both caption and image at the same time so it is together
+                contentStackLayout.Children.Add(chlidLayout);
             }
         }
     }
