56c0e38198f5937cb7bfb89e255858694becd1dd
amy tran <amylunatran@gmail.com>
2022-12-03 15:53:35 -0600

    Created view

---
+++ BITS-App/Views/MultiplePostContent.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BITS_App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MultiplePostContent : ContentPage
+    {
+        public MultiplePostContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
