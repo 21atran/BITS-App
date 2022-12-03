dc8eb0dfd2adef8a686a10bcc4dd54dbcccc454b
Galaxy <grkalinich@gmail.com>
2022-12-03 16:23:37 -0600

    Changed View.

--- BITS-App/Views/MultiplePostContent.xaml.cs
+++ BITS-App/Views/MultiplePostContent.xaml.cs
@@ -3,12 +3,11 @@ using System.ComponentModel;
 
 namespace BITS_App.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MultiplePostContent : ContentPage
-    {
-        public MultiplePostContent()
-        {
+    public partial class MultiplePostContent : ContentPage {
+        public MultiplePostContent() {
             InitializeComponent();
+
+            
         }
     }
 }
