8d59ec4bd98faf612301b85b4ac27fd4d59d5d21
Galaxy <grkalinich@gmail.com>
2022-12-03 19:08:40 -0600

    Got IDs into a list from the main page

--- BITS-App/Views/MultiplePostContent.xaml.cs
+++ BITS-App/Views/MultiplePostContent.xaml.cs
@@ -1,13 +1,38 @@
 ﻿using HtmlAgilityPack;
-using System.ComponentModel;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
-namespace BITS_App.Views
-{
+namespace BITS_App.Views {
     public partial class MultiplePostContent : ContentPage {
+
         public MultiplePostContent() {
             InitializeComponent();
 
-            
+            Dispatcher.Dispatch(async () => await GetPostsAsync());
         }
+
+        public async Task GetPostsAsync() {
+            // gets URI for server counterpart to model
+            Uri uri = new Uri("https://gwhsnews.org/wp-json/wp/v2/posts");
+
+            List<int> info = new List<int>();
+
+            // attempts to make an HTTP GET request and deserialize it for easy access
+            try {
+                HttpResponseMessage response = await App.client.GetAsync(uri);
+                if (response.IsSuccessStatusCode) {
+                    string content = await response.Content.ReadAsStringAsync();
+                    List<Json.Post> postJson = JsonConvert.DeserializeObject<List<Json.Post>>(content);
+                    foreach(Json.Post post in postJson)
+                    {
+                        info.Add(post.id);
+                    }
+                    // create new function eventually 
+                }
+            } catch (Exception ex) {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+        }
+
     }
 }
