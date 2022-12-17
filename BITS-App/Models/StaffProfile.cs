c34f30eb7271d8105dd7c39b0cc40cfe487d7d83
lxnat <goodnighteu@gmail.com>
2022-12-17 17:49:56 -0600

    Added Bindings to StaffProfile model

---
+++ BITS-App/Models/StaffProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BITS_App.Models {
+	/// <summary>
+	/// Model representing a staff profile.
+	///</summary>
+	internal class StaffProfile : RestBase {
+		//probably need a constructor of some sort LMAO
+		
+		//constructor-esqe thing
+		public StaffProfile(int id) : base($"/wp/v2/staff_profile/{id}") { }
+
+		// BINDINGS
+		public string? Name => null;
+		public string? Bio => null;
+		public string? FeaturedMedia => null;
+
+		// METHODS
+
+	}
+}
+
