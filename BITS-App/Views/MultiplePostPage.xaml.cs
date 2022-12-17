f313f0e11ea8aa937fabe1acf85c1b4d97434e83
Galaxy <grkalinich@gmail.com>
2022-12-17 17:36:52 -0600

    Created Panel of Posts

--- BITS-App/Views/MultiplePostPage.xaml.cs
+++ BITS-App/Views/MultiplePostPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using HtmlAgilityPack;
 using Newtonsoft.Json;
+using System.ComponentModel;
 using System.Diagnostics;
 
 namespace BITS_App.Views {
@@ -9,13 +10,15 @@ namespace BITS_App.Views {
             InitializeComponent();
 
             Dispatcher.Dispatch(async () => await GetPostsAsync());
+
+            PropertyChanged += OnPropertyChanged;
         }
 
         public async Task GetPostsAsync() {
             // gets URI for server counterpart to model
             Uri uri = new Uri("https://gwhsnews.org/wp-json/wp/v2/posts");
 
-            List<int> info = new List<int>();
+            //List<int> info = new List<int>();
 
             // attempts to make an HTTP GET request and deserialize it for easy access
             try {
@@ -25,12 +28,28 @@ namespace BITS_App.Views {
                     List<Json.Post> postJson = JsonConvert.DeserializeObject<List<Json.Post>>(content);
                     foreach(Json.Post post in postJson)
                     {
-                        info.Add(post.id);
+                        //info.Add(post.id);
+                        PostPanelView add = new PostPanelView(post.id);
+                        PostPanelStack.Add(add);
                     }
                     // create new function eventually 
                 }
             } catch (Exception ex) {
                 Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (sender == BindingContext)
+            {
+                switch (e.PropertyName)
+                {
+                    // if the Content of the model changes, reload it
+                    case "Content":
+                        Dispatcher.Dispatch(async () => await GetPostsAsync());
+                        break;
+                }
             }
         }
 
--- BITS-App/Views/PostPanelView.xaml.cs
+++ BITS-App/Views/PostPanelView.xaml.cs
@@ -1,7 +1,13 @@
 namespace BITS_App.Views;
 
-public partial class PostPanelView : ContentView {
-	public PostPanelView() {
-		InitializeComponent();
-	}
+public partial class PostPanelView : ContentView
+{
+    public PostPanelView(int id)
+    {
+        InitializeComponent();
+
+        BindingContext = new Models.Post(id);
+        Dispatcher.Dispatch(async () => await ((Models.Post)BindingContext).RefreshAsync());
+
+    }
 }
