f84a5268fe3fcaa43c5a7dec26329f01b43dc287
Ethan Parab <59212466+MandarinPine18@users.noreply.github.com>
2023-01-14 15:32:01 -0600

    Quick fixes and cleanup on the post panels

--- BITS-App/Views/MultiplePostPage.xaml.cs
+++ BITS-App/Views/MultiplePostPage.xaml.cs
@@ -9,15 +9,11 @@ namespace BITS_App.Views {
             InitializeComponent();
 
             Dispatcher.Dispatch(async () => await GetPostsAsync());
-
-            PropertyChanged += OnPropertyChanged;
         }
 
         public async Task GetPostsAsync() {
             // gets URI for server counterpart to model
-            Uri uri = new Uri("https://gwhsnews.org/wp-json/wp/v2/posts");
-
-            //List<int> info = new List<int>();
+            Uri uri = new($"https://{App.BASE_URL}/wp-json/wp/v2/posts");
 
             // attempts to make an HTTP GET request and deserialize it for easy access
             try {
@@ -25,32 +21,15 @@ namespace BITS_App.Views {
                 if (response.IsSuccessStatusCode) {
                     string content = await response.Content.ReadAsStringAsync();
                     List<Json.Post> postJson = JsonConvert.DeserializeObject<List<Json.Post>>(content);
-                    foreach(Json.Post post in postJson)
-                    {
-                        //info.Add(post.id);
+                    foreach(Json.Post post in postJson) {
                         PostPanelView add = new PostPanelView(post.id);
                         PostPanelStack.Add(add);
                     }
-                    // create new function eventually 
+                    // TODO: create new function eventually
                 }
             } catch (Exception ex) {
                 Debug.WriteLine(@"\tERROR {0}", ex.Message);
             }
         }
-
-        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (sender == BindingContext)
-            {
-                switch (e.PropertyName)
-                {
-                    // if the Content of the model changes, reload it
-                    case "Content":
-                        Dispatcher.Dispatch(async () => await GetPostsAsync());
-                        break;
-                }
-            }
-        }
-
     }
 }
