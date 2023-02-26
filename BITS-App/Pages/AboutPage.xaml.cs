cd975d12e2e982e5b90048f48fa73843624505ed
21atran <114887613+21atran@users.noreply.github.com>
2023-02-25 18:11:15 -0600

    Fixed AboutPage images

--- BITS-App/Pages/AboutPage.xaml.cs
+++ BITS-App/Pages/AboutPage.xaml.cs
@@ -1,7 +1,13 @@
-﻿namespace BITS_App.Pages;
+﻿using System.Windows.Input;
+
+namespace BITS_App.Pages;
 
 public partial class AboutPage : ContentPage {
+
+    public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
     public AboutPage() {
         InitializeComponent();
+        BindingContext = this;
     }
 }
