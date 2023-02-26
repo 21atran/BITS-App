151088c372cb91317a9db4a9070dcdde4351989c
Galaxy <grkalinich@gmail.com>
2023-02-25 18:16:09 -0600

    Transfered to Collection View and DataTemplates (basically we did work this time)

--- BITS-App/Pages/HomePage.xaml.cs
+++ BITS-App/Pages/HomePage.xaml.cs
@@ -1,7 +1,10 @@
 namespace BITS_App.Pages;
+using BITS_App.ViewModels;
 
 public partial class HomePage : ContentPage {
 	public HomePage() {
 		InitializeComponent();
-	}
+        BindingContext = new PostsViewModel();
+        Dispatcher.Dispatch(async () => await ((PostsViewModel)BindingContext).RefreshAsync());
+    }
 }
