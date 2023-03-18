42e4b4062007f15519b0485bff8dd8c5b96aff08
AshyBoi <ashtonedakkunnathu7@gmail.com>
2023-03-17 23:34:48 -0500

    got the social media links to work correctly.

--- BITS-App/Pages/AboutPage.xaml.cs
+++ BITS-App/Pages/AboutPage.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System.Windows.Input;
+﻿using System.Diagnostics;
+using System.Windows.Input;
 
 namespace BITS_App.Pages;
 
 public partial class AboutPage : ContentPage {
 
-    public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
-
+    public ICommand Command => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+    
+    
     public AboutPage() {
         InitializeComponent();
         BindingContext = this;
