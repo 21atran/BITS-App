103e432e0b1fd64906cb01b5f1b1a816c13e53bb
Galaxy <grkalinich@gmail.com>
2023-04-15 15:09:15 -0500

    some formatting fixed

--- BITS-App/Pages/AboutPage.xaml.cs
+++ BITS-App/Pages/AboutPage.xaml.cs
@@ -7,7 +7,6 @@ public partial class AboutPage : ContentPage {
 
     public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
     
-    
     public AboutPage() {
         InitializeComponent();
         BindingContext = this;
